4c21cd85bfca8da7e49ea2c06e286a1918138f59
you21979 <you2197901@gmail.com>
2014-03-20 15:26:10 +0900

    add install

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/Lemmings/lemmings-commander.git
+git clone https://github.com/Lemmings/lemmings-tactics-io.git
+git clone https://github.com/Lemmings/lemmings-tactics-task.git
+
